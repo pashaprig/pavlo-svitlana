a4e931c27162567631944d3e40f703a3a85261be
Pavlo Pryharnytskyi <66119998+pashaprig@users.noreply.github.com>
2026-09-26 17:16:15 +0300

    Delete js/guests.js

--- js/guests.js
+++
@@ -1,9 +0,0 @@
-const GUESTS = [
-  { lastName: 'Пригарницькі', video: 'assets/videos/petrenko.mp4' },
-  { lastName: 'Коваленко', video: 'assets/videos/kovalenko.mp4' },
-  { lastName: 'Шевченко', video: 'assets/videos/shevchenko.mp4' },
-  { lastName: 'Бойко', video: 'assets/videos/boiko.mp4' },
-  { lastName: "Мар'яненко", video: 'assets/videos/marianenko.mp4' },
-];
-
-const DEFAULT_VIDEO = 'assets/videos/promo.mp4';
